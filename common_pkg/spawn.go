27762f7668a32094a25d45f408d9f213a0198fe0
Alexis Lasher <alexis.lasher@apptreesoftware.com>
2019-05-28 10:11:22 -0700

    added a step to common for spawning a child workflow

---
+++ common_pkg/spawn.go
@@ -0,0 +1,33 @@
+package main
+
+import "github.com/apptreesoftware/go-workflow/pkg/step"
+
+type SpawnWorkflowInput struct {
+	Workflow    string
+	TriggerBody string
+}
+
+type SpawnWorkflow struct {
+
+}
+
+func (SpawnWorkflow) Name() string {
+	return "spawn_workflow"
+}
+
+func (SpawnWorkflow) Version() string {
+	return "1.0"
+}
+
+func (SpawnWorkflow) Execute(in step.Context) (interface{}, error) {
+	input := SpawnWorkflowInput{}
+	err := in.BindInputs(&input)
+	if err != nil {
+		return nil, err
+	}
+
+	engine := in.Engine()
+	err = engine.AddToQueue(input.Workflow, input.TriggerBody)
+	return nil, err
+}
+
