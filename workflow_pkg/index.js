431e5eedd31fcca2c224c4af8a1c219e054005e2
Matthew Smith <matthew.smith@apptreesoftware.com>
2019-08-28 11:39:53 -0700

    Starting workflow package

---
+++ workflow_pkg/index.js
@@ -0,0 +1,20 @@
+let apptree = require('../../go-workflow/node');
+
+apptree.addStep('exit', '1.0', exitWorkflowSuccess());
+apptree.addStep('fail', '1.0', exitWorkflowFail());
+
+apptree.run();
+
+function exitWorkflowSuccess() {
+    if (apptree.debug) {
+        console.log(`Exiting with code ${apptree.EXIT_WORKFLOW_SUCCESS_CODE}`);
+    }
+    process.exit(apptree.EXIT_WORKFLOW_SUCCESS_CODE);
+}
+
+function exitWorkflowFail() {
+    if (apptree.debug) {
+        console.log(`Exiting with code ${apptree.EXIT_WORKFLOW_FAIL_CODE}`);
+    }
+    process.exit(apptree.EXIT_WORKFLOW_FAIL_CODE);
+}
