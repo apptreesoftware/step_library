bff3b6906b037136c02f3b0e55c1ce0146345dd6
Abe <abe.foster@apptreesoftware.com>
2019-08-30 19:27:49 -0700

    ems_pkg first draft

--- ems_pkg/index.js
+++ ems_pkg/index.js
@@ -1,88 +1,71 @@
+//Steps to access the EMS WebServices
+// 8.30.2019
+// abe.foster@apptreesoftware.com
 let apptree = require('apptree-workflow-sdk');
-
-apptree.addStep('get_templates', '1.0', getTemplates());
+let axios = require('axios');
+apptree.addStep('get_templates', '1.0', getTemplates);
 
 apptree.run();
 
-let tokens = {};
-tokens.clientToken = null;
-tokens.apiToken = null;
-
-function getTemplates(inputs) {
-    apptree.validateInputs('HostUrl', 'ClientId', 'Secret', 'Username', 'Password');
+async function getTemplates(inputs) {
+    apptree.validateInputs('HostUrl', 'ClientId', 'Secret');
     const host = inputs['HostUrl'];
     const clientId = inputs['ClientId'];
     const secret = inputs['Secret'];
-    const username = inputs['Username'];
-    const password = inputs['Password'];
-    const endpoint = "platform/api/v1/webtemplates";
-    const request = new XMLHttpRequest();
+    const endpoint = "/platform/api/v1/webtemplates";
     const url = buildUrl(host, endpoint);
-    let response = "";
+    let templates = [];
 
-     tokens = authorize(host, clientId, secret, username, password);
+    let clientToken = await getClientToken(host, clientId, secret);
 
-    request.open("GET", url);
-    request.send();
-
-    request.onreadystatechange=function(){
-        if(this.readyState === XMLHttpRequest.DONE && this.status ===200){
-            response = request.responseText;
-            return{'Status': request.status, "Records": request.body};
-        }else{
-            return{"Status": request.status, "Message": request.responseText};
-        }
-    }
+    let config = {
+        headers: {'x-ems-api-token': clientToken}
+    };
+    await axios.get(url, config)
+        .then(function(response){
+            templates = response.data.results;
+        })
+        .catch(function (error) {
+            throw(error);
+        });
+    return {'Templates' : templates};
 }
 
-function authorize(host, clientId, secret, username, password){
-    let clientResponse = getClientToken(host, clientId, secret);
-    if(clientResponse['Status'] === "Success"){
-        getAuthToken(host, username, password, tokens.clientToken);
-    }
-}
-
-function getClientToken(host, clientId, secret){
+async function getClientToken(host, clientId, secret) {
     const url = buildUrl(host, "/platform/api/v1/clientauthentication");
     const creds = {clientId: clientId, secret: secret};
-    const clientRequest = new XMLHttpRequest();
-
-    clientRequest.open("POST", url);
-    clientRequest.setRequestHeader("Content-Type", "application/json");
-
-    clientRequest.onreadystatechange=function() {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            var json = JSON.parse(clientRequest.responseText);
-            tokens.clientToken = json.clientToken;
-            return{"Status": "Success"};
-        } else {
-            return {"Status": clientRequest.status, "Message": clientRequest.responseText};
-        }
+    let clientToken = "";
+    let config = {
+        headers: {'Content-Type': 'application/json'}
     };
-    clientRequest.send(JSON.stringify(creds));
+    await axios.post(url, JSON.stringify(creds), config)
+        .then(function (response) {
+            clientToken = response.data['clientToken'];
+        })
+        .catch(function (error) {
+            throw(error);
+        });
+    return clientToken;
 }
 
-function getAuthToken(host, username, pw, token){
+//Use this if the Call requires an Auth Token
+async function getAuthToken(host, username, pw, token){
     const url = buildUrl(host, "/platform/api/v1/authentication");
     const emsToken = token;
-    const authRequest = new XMLHttpRequest();
     const creds = {username: username, password: pw};
+    let authToken = "";
 
-    authRequest.open("POST", url);
-    authRequest.setRequestHeader("Content-Type", "application/json");
-    authRequest.setRequestHeader("x-ems-api-token", emsToken);
-
-    authRequest.onreadystatechange=function() {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            var json = JSON.parse(authRequest.responseText);
-            tokens.authToken = json['x-ems-api-token'];
-            return{"Status": "Success"}
-        } else {
-            return {"Status": clientRequest.status, "Message": clientRequest.responseText};
-        }
+    let config = {
+        headers: {'Content-Type': 'application/json','x-ems-api-token': emsToken}
     };
-
-    authRequest.send(JSON.stringify(creds));
+    await axios.post(url, JSON.stringify(creds), config)
+        .then(function(response){
+            authToken = JSON.parse(response.data['webToken']);
+        })
+        .catch(function (error) {
+            throw(error);
+        });
+    return authToken;
 }
 
 function buildUrl(host, endpoint){
